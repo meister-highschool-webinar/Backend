8ea7067fa8cce742495123f74b24e12517771e77
startergate <startergate36@gmail.com>
2020-08-18 00:14:27 +0900

    improvement(type): 타입 체크 추가
    path parameter에 대한 타입 체크를 추가했습니다.

--- webinar-api/controllers/webinar.controller.js
+++ webinar-api/controllers/webinar.controller.js
@@ -1,9 +1,17 @@
 const { Sequelize } = require('sequelize');
+const Joi = require('joi');
 
 const { webinarTable } = require('../models')
 
 exports.getWebinar = async (req, res) => {
   try {
+    const pathParam = Joi.object({
+      id: Joi.number().integer().required()
+    });
+    if (pathParam.validate(req.body).error) {
+      res.sendStatus(404);
+      return;
+    }
     const webinar = await webinarTable.findByPk(req.params.id, {
       attributes: ['title', 'link', 'detail']
     });
