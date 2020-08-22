8beb2dbe58a770c39689660dd43905407bfa5bf1
startergate <startergate36@gmail.com>
2020-08-22 15:50:37 +0900

    feat: 유저 토큰 인증

--- webinar-api/modules/chat.js
+++ webinar-api/modules/chat.js
@@ -1,23 +1,25 @@
+const {verifyJWT} = require("../middlewares/auth.middle");
 const { io } = require('../modules/websocket');
 
 let connected = 0;
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
+  const token = socket.request._query['token'];
+  const payload = await verifyJWT(token)
+  socket.user_id = payload.id;
   connected++;
-  io.to(socket.id).emit('create name', connected);
-  io.emit('connected_change', connected);
+  io.emit('connected_change', ++connected);
 
-  socket.on('disconnect', function(){
-    io.emit('connected_change', connected);
+  socket.on('disconnect', _ => {
+    io.emit('connected_change', --connected);
   });
 
-  socket.on('send message', function(text){
+  socket.on('send message', (text) => {
     const time = Date.now()
     io.emit('receive message', {
-      user_id: id,
+      user_id: socket.user_id,
       name: socket.name,
       text: text,
       time: time
     });
   });
-
 });
