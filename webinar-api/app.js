db63a42854355e551a03e4abcf1550ac3e567f68
leafcis <leaf_cis@naver.com>
2020-09-13 16:57:39 +0900

    fix(cors): pre-flight 허용

--- webinar-api/app.js
+++ webinar-api/app.js
@@ -11,6 +11,7 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(cors());
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
